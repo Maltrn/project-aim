8c7cfcf7f4fff1d97d3cd7d4c4d1e44407fe74d1
Daniel Schruhl <danielschruhl@gmail.com>
2016-10-31 13:40:38 +0100

    changes validator implementation

--- src/main/java/de/haw/aim/uploadcenter/persistence/Picture.java
+++ src/main/java/de/haw/aim/uploadcenter/persistence/Picture.java
@@ -1,23 +1,58 @@
 package de.haw.aim.uploadcenter.persistence;
 
+import de.haw.aim.validator.ValueDoesntValidateToConfigFileException;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Picture implements File {
-    @Override
-    public boolean isValid() {
-        return false;
+
+    @Id
+    protected String id;
+
+    protected String name;
+
+    protected String location;
+
+    public Picture() {
     }
 
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public String getLocation() {
-        return null;
+    public Picture(String filepath) {
+        this.location = filepath;
+        this.name = this.location.substring(0, this.location.length() - 4);
     }
 
     @Override
     public String getId() {
-        return null;
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public void validate() throws ValueDoesntValidateToConfigFileException {
+        if (!this.location.endsWith(".pdf")) {
+            throw new ValueDoesntValidateToConfigFileException("Wrong file ending");
+        }
     }
 }
