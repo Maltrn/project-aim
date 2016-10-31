3150b943eb1e5ae5a3055e0d13e21f98f4c3683c
Rene Kretschmer <rene.kretschmer@haw-hamburg.de>
2016-10-31 13:26:56 +0100

    Info Test added

---
+++ src/test/java/de/haw/aim/vendor/InfoTest.java
@@ -0,0 +1,23 @@
+package de.haw.aim.vendor;
+
+import de.haw.aim.uploadcenter.persistence.File;
+import de.haw.aim.uploadcenter.persistence.PDF;
+import de.haw.aim.vendor.persistence.ProductInfo;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class InfoTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @Test
+    public void tryToSaveInfos()
+    {
+//        ProductInfo productInfo = new ProductInfo("Produkt");
+    }
+}
