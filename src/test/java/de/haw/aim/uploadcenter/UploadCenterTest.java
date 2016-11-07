e4e8e6c7849c37df19c10ad059b299c646342378
Jana Wengenroth <jana.wengenroth@haw-hamburg.de>
2016-11-07 14:11:40 +0100

    delete file test add

--- src/test/java/de/haw/aim/uploadcenter/UploadCenterTest.java
+++ src/test/java/de/haw/aim/uploadcenter/UploadCenterTest.java
@@ -1,22 +1,18 @@
 package de.haw.aim.uploadcenter;
 
-import de.haw.aim.uploadcenter.facade.IUploadCenter;
+import de.haw.aim.uploadcenter.persistence.PDFRepository;
+import de.haw.aim.uploadcenter.persistence.PictureRepository;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.util.Assert;
-import org.springframework.web.multipart.MultipartFile;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Paths;
-
-import static org.testng.Assert.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,31 +21,46 @@ public class UploadCenterTest extends AbstractTestNGSpringContextTests
     @Autowired
     private UploadCenter uploadCenter;
 
+    @Autowired
+    private PDFRepository pdfRepository;
+
+    @Autowired
+    private PictureRepository pictureRepository;
+
     @BeforeMethod
     public void setUp() throws Exception
     {
         FileUtils.cleanDirectory(Paths.get("./src/test/resources/testFiles").toFile());
+        pictureRepository.deleteAll();
+        pdfRepository.deleteAll();
     }
 
     @Test
     public void testUploadFile() throws Exception
     {
         MockMultipartFile file = new MockMultipartFile("b", "b.png", "image/png", "nonsensecontent".getBytes());
-        this.uploadCenter.uploadFile(file);
+        de.haw.aim.uploadcenter.persistence.File persistedFile = this.uploadCenter.uploadFile(file);
         File newFile = new File("./src/test/resources/testFiles/b.png");
         Assert.isTrue(newFile.exists());
+        Assert.isTrue(pictureRepository.exists(persistedFile.getId()));
     }
 
     @Test
     public void testReplaceFile() throws Exception
     {
-
+        MockMultipartFile file = new MockMultipartFile("b", "b.png", "image/png", "nonsensecontent".getBytes());
+        MockMultipartFile file2 = new MockMultipartFile("a", "a.png", "image/png", "nonsensecontent".getBytes());
     }
 
     @Test
     public void testDeleteFile() throws Exception
     {
-
+        MockMultipartFile file = new MockMultipartFile("b", "b.png", "image/png", "nonsensecontent".getBytes());
+        de.haw.aim.uploadcenter.persistence.File deleteThis = this.uploadCenter.uploadFile(file);
+        this.uploadCenter.deleteFile(deleteThis.getId());
+        File newFile = new File("./src/test/resources/testFiles/b.png");
+        Assert.isTrue(!newFile.exists());
+        Assert.isTrue(!pictureRepository.exists(deleteThis.getId()));
     }
 
     @Test
