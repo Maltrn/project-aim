d0d40f70d3e50a2cdaf092a44ead3d84decab297
Maltrn <maltron.tc@gmail.com>
2016-11-21 10:43:46 +0100

    Update

--- frontend/src/app/informationComponents/main/vendor.service.ts
+++ frontend/src/app/informationComponents/main/vendor.service.ts
@@ -4,14 +4,21 @@
 import {Injectable} from "@angular/core";
 import {Headers, Http} from "@angular/http";
 import 'rxjs/add/operator/toPromise';
+import {InfoDTO} from "./dto/infoDTO";
 
 @Injectable()
 export class VendorService
 {
+    private vendorUrl: String = '/vendor'
 
     constructor(private http: Http)
     {
     }
 
-    getVendorInformation(): Prom
+    getVendorInformation(vendorId: String): Promise<InfoDTO>
+    {
+        return this.http.get(this.vendorUrl + '/' + vendorId)
+            .toPromise()
+            .then(response => response.json().data as InfoDTO);
+    }
 }
