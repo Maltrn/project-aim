95072d7cb7156c2c16e9c4d0addfa640336e49ae
Maltrn <maltron.tc@gmail.com>
2016-11-28 12:05:48 +0100

    User erstellt

--- frontend/src/app/authentification/model/loginDTO.ts
+++ frontend/src/app/authentification/model/loginDTO.ts
@@ -1,3 +1,30 @@
 /**
  * Created by Malte Scheller on 28.11.16.
  */
+
+export class LoginDTO
+{
+    private _username: string;
+    private _password: string;
+
+
+    get username(): string
+    {
+        return this._username;
+    }
+
+    set username(value: string)
+    {
+        this._username = value;
+    }
+
+    get password(): string
+    {
+        return this._password;
+    }
+
+    set password(value: string)
+    {
+        this._password = value;
+    }
+}
