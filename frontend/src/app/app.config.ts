2e6b0627f75111d7da4d2125a7079e8aefebba72
Daniel Schruhl <danielschruhl@gmail.com>
2016-11-28 12:34:12 +0100

    loading config importer as injectable globally

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,34 +1,21 @@
-import {Injectable} from "@angular/core"
+import {Injectable} from "@angular/core";
+import fs = module("fs")
 
 @Injectable()
 export class ConfigImporter {
 
-    private fileLocation:String = "src/main/resources/config.properties"
-
-    private fs = require('fs');
+    private fileLocation: String = "src/main/resources/config.properties"
 
     constructor() {
         this.showFile();
     }
 
     showFile() {
-
-        this.fs.readFile(this.fileLocation, function (err, data) {
+        fs.readFile(this.fileLocation, function (err, data) {
             if (err) {
                 return console.error(err);
             }
             console.log("Asynchronous read: " + data.toString());
         });
     }
-    // readThis() : void {
-    //     var file:File = new File(this.fileLocation);
-    //     var myReader:FileReader = new FileReader();
-    //
-    //     myReader.onloadend = function (e) {
-    //         // you can perform an action with readed data here
-    //         console.log(myReader.result);
-    //     }
-    //
-    //     myReader.readAsText(file);
-    // }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,8 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import {HttpModule} from "@angular/http";
-
 import {AppComponent} from "./app";
 import {rootRouterConfig} from "./app.routes";
-
-// Vendor-Info
 import {AddFilesComponent} from "./informationComponents/add-files/add-files.component";
 import {DetailedDiscriptionComponent} from "./informationComponents/detailed-description/detailed-discription.component.ts";
 import {FactsTableComponent} from "./informationComponents/facts/facts.component";
@@ -18,18 +15,20 @@ import {SortFilesComponent} from "./informationComponents/sort-files/sort-files.
 import {InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {ProfilePictureService} from "./informationComponents/profile-pic/profile-pic.service.ts";
 import {InMemoryDataService} from "./informationComponents/profile-pic/in-memory-data.service";
-
-// Product-Info
 import {ProductInfoMain} from "./informationComponents/main/product-info-main.component";
-import  {UserLogin} from "./authentification/user.component"
-
-// Upload-Center
+import {UserLogin} from "./authentification/user.component";
 import {UploadCenter} from "./uploadCenter/uploadCenter";
 import {FileService} from "./uploadCenter/file.service";
 import {VendorService} from "./informationComponents/main/vendor.service";
 import {UserService} from "./authentification/user.service";
 import {LoggedInGuard} from "./authentification/loged-in.guard";
+import {ConfigImporter} from "./app.config";
 
+// Vendor-Info
+
+// Product-Info
+
+// Upload-Center
 
 
 @NgModule({
@@ -54,13 +53,14 @@ import {LoggedInGuard} from "./authentification/loged-in.guard";
         UserLogin
     ],
     bootstrap: [AppComponent],
-    providers: [FileService,
-                ProfilePictureService,
-                VendorService,
-                UserService,
-                LoggedInGuard]
+    providers: [
+        FileService,
+        ProfilePictureService,
+        VendorService,
+        UserService,
+        LoggedInGuard,
+        ConfigImporter]
 })
 
-export class AppModule
-{
+export class AppModule {
 }
