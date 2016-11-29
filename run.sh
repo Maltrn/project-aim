788342f37227214f6d51a38a7304e9742ce39228
Christof <wowcr@gmx.net>
2016-11-29 21:48:42 +0100

    added mongorestore to run.sh for docker container

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Start MongoDB
 /usr/bin/mongod --quiet --config /etc/mongod.conf &
+# Import testdata
+mongorestore --host localhost:27017 --db test ./dumpdir/test
 # Start Backend
 java -jar ./build/libs/project-aim.jar &
 # Start Frontend
