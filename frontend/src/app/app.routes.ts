c8a951e3b3065d0c05b0a67875b9c8a88e50d07b
Maltrn <maltron.tc@gmail.com>
2016-12-12 12:45:08 +0100

    added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import {Routes} from "@angular/router";
+import {UserLogin} from "./authentification/user.component";
 
 export const rootRouterConfig: Routes = [
-  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  //{path: 'upload-center', component: UploadCenter},
+  //{path: 'anbieterinformationen', component: VendorInfoMain},
+  //{path: 'produktinformationen', component: ProductInfoMain},
+  {path: 'login', component: UserLogin}
 ];
 
