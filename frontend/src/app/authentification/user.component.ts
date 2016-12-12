e5ebe56d52e62f7bdec5ed720a94b48d15c8fa51
Maltrn <maltron.tc@gmail.com>
2016-12-12 12:57:28 +0100

    WIP

--- frontend/src/app/authentification/user.component.ts
+++ frontend/src/app/authentification/user.component.ts
@@ -1,47 +1,27 @@
 import {Component} from "@angular/core";
-import {UserService} from "./user.service.ts";
-import {Router} from "@angular/router";
-import {LoginRequest} from "./model/loginRequest";
-import {Response} from "@angular/http";
+import {UserService} from "./user.service";
+import {LoginDTO} from "./model/loginDTO";
 
 @Component
 ({
-    selector: 'login',
-    templateUrl: './view/userview.component.html',
+  selector: 'login',
+  templateUrl: './view/userview.component.html',
 })
 
 export class UserLogin
 {
-    constructor(private userService: UserService, private router: Router){}
+  private user = {};
 
-    /**
-     *
-     * @param email
-     * @param password
-     */
-    onSubmit(email, password)
-    {
-        var loginDto: LoginRequest =
-        {
-            password: password,     //TODO Eingaben mit Regex ueberpruefen
-            username: email
-        };
+  constructor(private userService: UserService)
+  {
+  }
 
-        this.userService.login(loginDto);
-    }
-
-    test1(): void
-    {
-        console.log(localStorage.getItem('aim_token'));
-    }
-
-    test2(): void
-    {
-        console.log(localStorage.getItem('aim_vendorId'));
-    }
-
-    logout(): void
-    {
-        console.log(this.userService.logout());
-    }
+  public login(username: string, password: string)
+  {
+    let loginBody: LoginDTO = new LoginDTO(username, password);
+    this.userService.login().subscribe(
+      data => this.user = data,
+      error => console.log("ERROR in REST API")
+    );
+  }
 }
--- frontend/src/app/authentification/user.service.ts
+++ frontend/src/app/authentification/user.service.ts
@@ -1,10 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers, Response, RequestOptions} from '@angular/http';
-import {LoginRequest} from "./model/loginRequest";
-import {User} from "./model/user";
+import {LoginDTO} from "./model/loginDTO";
+import {Configs} from "../app.config";
+import {Observable} from "rxjs";
 
-const loginUrl = 'http://localhost:8080/api/login';
-const mockUrl = 'api/auth';
 
 const errMsg401 = 'Benutzername existiert nicht oder das angegebene Passwort ist falsch';
 const errMsg503 = 'Login nicht erfolgreich, da keine Anbieter und/oder Produktinformationen abgerufen werden können.';
@@ -13,109 +12,47 @@ const unknownErrMsg = 'Unbekannter Fehler';
 @Injectable()
 export class UserService
 {
-    private _loggedIn: boolean;       // for CanActivate guard
-    private _user: User;
 
-    constructor(private http: Http)
+  constructor(private http: Http)
+  {
+
+  }
+
+  /**
+   * Sending the user.json credentials to the backend.
+   * If the credentials are valid, the response contains a token and user.json data.
+   * User data and token will be store in the local storage and "loggedIn" will be set to true.
+   * */
+  public login()
+  {
+    let body = JSON.stringify({"password": "1234", "username": "asd"});
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers, method: "post"});
+
+    console.log(Configs.BACKEND_URL + "login");
+    console.log(body);
+
+    this.http.post('http://aim.gartsy.de/api/login', body, options);
+
+    return this.http.post('http://aim.gartsy.de/api/login', body, options)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response | any)
+  {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if(error instanceof Response)
     {
-        this._loggedIn = false;
-
-        //this._user.vendorInfoId = JSON.parse(localStorage.getItem('aim_vendorId'));
-    }
-
-    /**
-     * Sending the user.json credentials to the backend.
-     * If the credentials are valid, the response contains a token and user.json data.
-     * User data and token will be store in the local storage and "loggedIn" will be set to true.
-     * */
-    login(loginRequest: LoginRequest): void
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else
     {
-        let headers = new Headers({'Content-Type': 'application/json'});
-        let options = new RequestOptions({headers: headers});
-
-        this.http
-            .post(loginUrl, JSON.stringify(loginRequest), options)
-            .toPromise()
-            .then(res =>
-            {
-                this._user = res.json().data as User;
-            })
-            .then(() =>
-            {
-                localStorage.setItem('aim_token', this._user.loginResponse.currentToken);
-                localStorage.setItem('aim_user', this._user.loginResponse.currentToken);
-
-                console.log(this._user);
-            })
-            .then(res =>
-            {
-                if(res)
-                {
-                    this._loggedIn = true;
-                }
-
-            }).catch(this.handleError);
+      errMsg = error.message ? error.message : error.toString();
     }
-
-    /**
-     * Delete the user.json data and token from local storage and set loggedIn to false.
-     */
-    logout()
-    {
-        localStorage.removeItem('aim_token');
-        localStorage.removeItem('aim_vendorId');
-        this._loggedIn = false;
-    }
-
-    isLoggedIn(): boolean
-    {
-        return this._loggedIn;
-    }
-
-    get user(): User
-    {
-        return this._user;
-    }
-
-    get token(): string
-    {
-        return localStorage.getItem('aim_token');
-    }
-
-    handleError(error: any): void
-    {
-        let errMsg: string;
-
-        if(error instanceof Response)
-        {
-            let resErr: Response = error;
-            errMsg = `${resErr.status} - ${resErr.statusText || ''} ${resErr}`;
-
-            switch(resErr.status)
-            {
-                case 401:
-                    errMsg = errMsg401;
-                    break;
-                case 503:
-                    errMsg = errMsg503;
-                    break;
-                default:
-                    errMsg = unknownErrMsg;
-            }
-        }
-        else
-        {
-            errMsg = error.toString();
-        }
-
-        console.error('An error occurred in User-Service', errMsg);
-
-        if((errMsg !== '') && (errMsg !== null))      //TODO ErrorMsg direkt im Eingabebereich anzeigen. PopUp evtl.
-        {
-            console.log(errMsg);
-            console.log("Detail: " + error);
-        }
-        else
-            console.log('Fehler nicht beschrieben');  //TODO Beschreibung anpassen
-    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
