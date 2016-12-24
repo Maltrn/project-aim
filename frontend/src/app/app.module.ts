dcd2de99409056dacc8c3e551acefc43bcd20d8a
Daniel Schruhl <danielschruhl@gmail.com>
2016-12-24 12:15:38 +0100

    Add pipe to format file sizes in bytes to kb or mb

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,6 +23,7 @@ import {FileService} from "./uploadCenter/file.service";
 import {UploadCenter} from "./uploadCenter/uploadCenter";
 import {AuthGuard} from "./authentification/user.authguard";
 import {Settings} from "./app.config";
+import {FileSizePipe} from "./uploadCenter/file-size.pipe";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import {Settings} from "./app.config";
     ShortDiscriptionComponent,
     SortFilesComponent,
     ProfilePicComponent,
-    UploadCenter
+    UploadCenter,
+    FileSizePipe
   ],
   imports: [
     BrowserModule,
