a93899a0dc72a25e237c1e7810e50dceb2b8b530
Daniel Schruhl <danielschruhl@gmail.com>
2016-12-24 02:19:39 +0100

    Hide Navigation if not logged in

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,6 +8,6 @@ import {UserService} from "./authentification/user.service";
 export class AppComponent {
 
   constructor(private userService: UserService) {
-
+    console.log(localStorage.getItem('user'));
   }
 }
