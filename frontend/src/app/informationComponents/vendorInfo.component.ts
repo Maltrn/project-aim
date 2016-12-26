e7d1aeac24e06874dde1f7a0c48407df71bb17d2
Daniel Schruhl <danielschruhl@gmail.com>
2016-12-26 14:48:23 +0100

    Add short description to vendor edit view

--- frontend/src/app/informationComponents/vendorInfo.component.ts
+++ frontend/src/app/informationComponents/vendorInfo.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {VendorService} from "./vendor.service";
 import {UserService} from "../authentication/user.service";
+import {Settings} from "../app.config";
 
 
 @Component
@@ -11,10 +12,24 @@ import {UserService} from "../authentication/user.service";
 
 export class VendorInfo {
 
-  private vendor;
+  private vendor: any = {
+    "id": "",
+    "name": "",
+    "shortDescription": "",
+    "longDescription": "",
+    "mainPic": "",
+    "fileGallery": [],
+    "facts": []
+  };
 
-  constructor(private vendorService: VendorService, private userService: UserService) {
+  private description: string;
 
+  private shortDescription: any;
+
+  private renderShortDescription: boolean;
+
+  constructor(private vendorService: VendorService, private userService: UserService, private settings: Settings) {
+    this.renderShortDescription = false;
   }
 
   ngOnInit(): void {
@@ -23,12 +38,53 @@ export class VendorInfo {
 
   private loadVendor(id: string) {
     this.vendorService.getVendor(id).subscribe(
-      data => this.vendor = data,
+      data => {
+        this.vendor = data;
+        this.renderShortDescription = true;
+        this.updateShortDescriptionText();
+      },
       error => {
         console.log("ERROR in REST API");
         if (error.indexOf("401") !== -1) {
           this.userService.logout();
         }
       });
+  }
+
+  // events
+  public onChange(e: any): void {
+    this.updateShortDescriptionText();
+    console.log(e);
+  }
+
+  public onReady(e: any): void {
+    this.updateShortDescriptionText();
+    console.log(e);
+  }
+
+  public onFocus(e: any): void {
+    this.updateShortDescriptionText();
+    console.log(e);
+  }
+
+  public onBlur(e: any): void {
+    this.updateShortDescriptionText();
+    console.log(e);
+  }
+
+  private updateShortDescriptionText(): void {
+    this.description = 'Maximale Anzahl der Zeichen: ';
+    let actualLength = this.sanitizedTextLenght(this.vendor.shortDescription);
+    if (actualLength > this.settings.shortDescriptionMaxLength) {
+      this.description += "<span class=\"text-danger\">" + actualLength + "</span>";
+    } else {
+      this.description += actualLength;
+    }
+    this.description += '/' + this.settings.shortDescriptionMaxLength;
+  }
+
+  private sanitizedTextLenght(text: string): number {
+    let sanitized: string = text.replace(/(<(?:.|\n)*?>|(?:\r\n|\r|\n))/gm, '');
+    return sanitized.length;
   }
 }
